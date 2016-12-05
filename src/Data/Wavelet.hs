68f1ea54aa8903d393b5c23c934e6b8c07444ae8
John Haynes <john.andrew.haynes@gmail.com>
2016-12-05 22:02:36 +0100

    implemented Wavelet Matrix 'Access'

--- src/Data/Wavelet.hs
+++ src/Data/Wavelet.hs
@@ -7,7 +7,7 @@ import Data.Wavelet.Internal.Types
 class Wavelet structure where
 
     {- Return the element at a given position -}
-    access :: structure -> Position -> a
+    access :: Bits a => structure -> Position -> a
 
     {- Count the occurrences of a given symbol up to a given position -}
     rank :: Bits a => structure -> a -> Position -> Count
